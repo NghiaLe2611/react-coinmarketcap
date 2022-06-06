c256cc4c71144ec9524a57b95981e0297d7fe2d8
Nghia Le <nghiapro2611@gmail.com>
2022-06-06 23:06:11 +0700

    Commit
    fix

--- server/server.js
+++ server/server.js
@@ -16,12 +16,11 @@ if (process.env.NODE_ENV === 'production') {
 	app.use(express.static(path.join(__dirname, 'client/build')));
 	// Handle React routing, return all requests to React app
 	app.get('*', function (req, res) {
-		res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+		res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 	});
 }
 
 app.get('/api/status', function(req, res) {
-    console.log(path.join(__dirname, '../client/build', 'index.html'));
     return res.status(200).json({
         status: true
     })
