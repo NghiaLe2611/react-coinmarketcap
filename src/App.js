b45de022b16fe15d3b84db1e770bcd5d29f87520
Nghia Le <nghiapro2611@gmail.com>
2022-06-06 17:02:59 +0700

    Commit
    First commit (test)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [status, setStatus] = useState(false);
+	useEffect(() => {
+		axios.get(`${process.env.REACT_APP_API_URL}/api/status`)
+			.then(function (response) {
+				if (response.data.status) {
+					setStatus(true);
+				}
+			})
+			.catch(function (error) {
+				console.log(error);
+			})
+	}, []);
+
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				<img src={logo} className='App-logo' alt='logo' />
+				{status && <p><strong>OK</strong></p>}
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
+					Learn React
+				</a>
+			</header>
+		</div>
+	);
 }
 
 export default App;
