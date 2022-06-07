74aab47d02ae02164b45a61da73fd4b7b0248b11
Nghia Le <nghiapro2611@gmail.com>
2022-06-07 21:43:56 +0700

    Commit
    fix

--- server.js
+++ server.js
@@ -11,15 +11,6 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-if (process.env.NODE_ENV === 'production') {
-	// Serve any static files
-    app.use(express.static('client/build'));
-	// Handle React routing, return all requests to React app
-	app.get('*', function (req, res) {
-		res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-	});
-}
-
 app.get('/api/status', function(req, res) {
     return res.status(200).json({
         status: true
@@ -33,5 +24,15 @@ app.use((error, req, res, next) => {
 		},
 	});
 });
+
+// This route must be appeared after at all other routes
+if (process.env.NODE_ENV === 'production') {
+	// Serve any static files
+    app.use(express.static('client/build'));
+	// Handle React routing, return all requests to React app
+	app.get('*', function (req, res) {
+		res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+	});
+}
 
 app.listen(port, () => console.log(`Server started at port ${port}`));
