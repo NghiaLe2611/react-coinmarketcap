f0ddc6ef4e1e03e314d40071a6338bd27b0822db
Nghia Le <nghiapro2611@gmail.com>
2022-09-04 23:22:22 +0700

    Commit
    Fix heroku (done)

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.get('/', function (req, res) {
+app.get('/api/status', function (req, res) {
 	return res.status(200).json({
 		success: true,
 	});
